e8fb71bbbe84836d6df3b4c236c1fc8daf13e779
Kanha Tomar <kanharajput91@gmail.com>
2022-03-02 10:57:46 +0530

    Transition applied when relauching the app

--- app/src/main/java/com/example/transitionsapp/MainActivity.java
+++ app/src/main/java/com/example/transitionsapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.transitionsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // don't need to add anything in theme it's already working fine
+        // set the enter transition
+        getWindow().setEnterTransition(new Explode());    // as we are relaunching this activity by itself
+
+        // set the exit transition
+        getWindow().setExitTransition(new Explode());
+
+    }
+
+    // relauching the activity with transitions
+    public void relaunchMainActivity(View view) {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 }
